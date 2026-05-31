e5496c4f92c92142d617e621292f5e6d7a31628f
rushu <rushurushu721@gmail.com>
2026-05-31 13:53:35 +0545

    Initial commit: Personal portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Navbar } from "@/layout/Navbar";
+import { Hero } from "@/sections/Hero";
+import { About } from "@/sections/About";
+import { Projects } from "@/sections/Projects";
+import { Testimonials } from "@/sections/Testimonials";
+import { Contact } from "@/sections/Contact";
+import { Footer } from "./layout/Footer";
+
+function App() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Testimonials />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
